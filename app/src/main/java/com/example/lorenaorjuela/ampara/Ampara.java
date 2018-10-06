f2a41900000f01464eaf87ea7850651f0324fdcf
LorenaOrjuela <42306567+LorenaOrjuela@users.noreply.github.com>
2018-10-05 21:02:25 -0500

    my primer commit v2

---
+++ app/src/main/java/com/example/lorenaorjuela/ampara/Ampara.java
@@ -0,0 +1,13 @@
+package com.example.lorenaorjuela.ampara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ampara extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ampara);
+    }
+}
